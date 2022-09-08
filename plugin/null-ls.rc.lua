57bedadfe54e6d113bd93d15331933b635929267
datsfilipe <datsfilipe.dev@gmail.com>
2022-09-08 00:09:08 -0300

    fix all direction moves at windows keymaps, add code actions keymap with null-ls and set cspell filetypes at null-ls config

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -7,9 +7,11 @@ null_ls.setup {
   sources = {
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.diagnostics.eslint_d.with({
-        diagnostics_format = '[eslint] #{m}\n(#{c})'
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
-    null_ls.builtins.diagnostics.cspell,
+    null_ls.builtins.diagnostics.cspell.with({
+      filetypes = { 'markdown' }
+    }),
     null_ls.builtins.diagnostics.gitlint
   },
   on_attach = function(client, bufnr)
