e3058a955224506aa9753dd02e16c540e3ad449e
datsfilipe <datsfilipe.dev@gmail.com>
2022-09-10 02:41:20 -0300

    move lines from init.lua to base.lua

--- init.lua
+++ init.lua
@@ -2,8 +2,3 @@ require 'dtsf.base'
 require 'dtsf.highlights'
 require 'dtsf.keymaps'
 require 'dtsf.plugins'
-
--- it will disable copilot on startup and avoid the delay of autocmd approach
-vim.g.copilot_enabled = 0
--- this will allow me to enable it iven if Tab key is used for comp.nvim
-vim.g.copilot_assume_mapped = 1
