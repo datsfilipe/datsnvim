b29a7cc720f5370f8bf1cdf2bbe418499df0d483
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-07 09:27:00 -0300

    refactor: use vim.lsp.buf.format instead of deprecating lsp format funcions

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -24,7 +24,9 @@ null_ls.setup {
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup_format,
         buffer = 0,
-        callback = function() vim.lsp.buf.formatting_seq_sync() end
+        callback = function()
+          vim.lsp.buf.format()
+        end
       })
     end
   end,
