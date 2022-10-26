7ab791261ef49630d5a20f017ded1caf5de8bca5
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-26 12:37:29 -0300

    fix: remove annoying lightbulb from lspsaga code action conf

--- plugin/lspsaga.lua
+++ plugin/lspsaga.lua
@@ -4,7 +4,14 @@ if not present then return end
 saga.init_lsp_saga {
   server_filetype_map = {
     typescript = 'typescript'
-  }
+  },
+  code_action_icon = '💡',
+  -- if true can press number to execute the codeaction in codeaction window
+  code_action_num_shortcut = true,
+  -- same as nvim-lightbulb but async
+  code_action_lightbulb = {
+    enable = false,
+  },
 }
 
 local opts = { noremap = true, silent = true }
