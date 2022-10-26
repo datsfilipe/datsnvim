d633d0884663f38ba3c85464725fcc5c87dfa73e
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-26 10:59:02 -0300

    fix: eslint_d formatting bultin not added to null-ls conf

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -17,6 +17,7 @@ null_ls.setup {
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.diagnostics.fish,
     -- null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.diagnostics.cspell.with({
