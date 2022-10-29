25fea24296748b0671e0506ed5469ca0488b0ff9
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-29 17:26:20 -0300

    feat: add tokyonight colorscheme

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -4,7 +4,8 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    -- theme = 'gruvbox',
+    theme = 'tokyonight',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
     disabled_filetypes = {}
---
+++ after/plugin/tokyonight.rc.lua
@@ -0,0 +1,45 @@
+local present, tokyonight = pcall(require, 'tokyonight')
+if not present then
+  return
+end
+
+tokyonight.setup({
+  transparent = true,
+  dim_inactive = false,
+  terminal_colors = false,
+  lualine_bold = true,
+  styles = {
+    comments = 'NONE',
+    functions = 'NONE',
+    keywords = 'NONE',
+    strings = 'NONE',
+    variables = 'NONE',
+    sidebars = 'transparent',
+    floats = 'transparent'
+  },
+  on_highlights = function(hl, c)
+    hl.LspDiagnosticsDefaultHint = { fg = c.blue }
+    hl.LspDiagnosticsDefaultInformation = { fg = c.blue }
+    hl.LspDiagnosticsDefaultWarning = { fg = c.yellow }
+    hl.LspDiagnosticsDefaultError = { fg = c.red }
+    -- remove telescope background
+    hl.TelescopeNormal = { bg = 'NONE' }
+    hl.TelescopeBorder = { bg = 'NONE' }
+    hl.TelescopePromptBorder = { bg = 'NONE' }
+    hl.TelescopeResultsBorder = { bg = 'NONE' }
+    hl.TelescopePreviewBorder = { bg = 'NONE' }
+  end,
+})
+
+vim.cmd[[colorscheme tokyonight-storm]]
+-- change highlight color for indent blankline
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#2d2e43 gui=NONE]]
+-- change highlight colors for telescope
+vim.cmd [[highlight TelescopePromptBorder guifg=#2d2e43 gui=NONE]]
+vim.cmd [[highlight TelescopePreviewBorder guifg=#2d2e43 gui=NONE]]
+vim.cmd [[highlight TelescopeResultsBorder guifg=#2d2e43 gui=NONE]]
+-- change cmp border color
+vim.cmd [[highlight CmpPmenuBorder guifg=#2d2e43]]
+vim.cmd [[highlight CmpDocPmenuBorder guifg=#2d2e43]]
+-- change highlight for float window background
+vim.cmd [[highlight NormalFloat guibg=NONE]]
