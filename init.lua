7f163bebaf404cd02f022271b6a133d1061bf62b
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-02 15:23:50 -0300

    fix(copilot): copilot.vim doesn't work with node v.18 yet, so it's a workaround

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require 'dtsf.options'
 require 'dtsf.bootstrap'
 require 'dtsf.plugins'
 require 'dtsf.keymaps'
+
+-- workaround for https://github.com/orgs/community/discussions/16800
+vim.g.copilot_node_command = "/home/dtsf/.asdf/installs/nodejs/17.9.1/bin/node"
