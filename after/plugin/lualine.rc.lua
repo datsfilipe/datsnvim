f3b6ab5b38e139e3a6b49fd625001327bc44e161
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-10 22:47:01 -0300

    refactor(theme): using gruvbox back

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -4,8 +4,8 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    -- theme = 'gruvbox',
-    theme = 'tokyonight',
+    theme = 'gruvbox',
+    -- theme = 'tokyonight',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
     disabled_filetypes = {}
