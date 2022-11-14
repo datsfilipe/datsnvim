c05137dda23382852ac04a313e13a6881bbf7180
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-13 22:43:42 -0300

    fix(copilot): messing up with copilot config trying to fix weird behaviors it is showing up

--- after/plugin/copilot.rc.lua
+++ after/plugin/copilot.rc.lua
@@ -2,3 +2,13 @@ vim.g.copilot_filetypes = {
   ['*'] = true,
   ['yaml'] = true,
 }
+
+-- it will disable copilot on startup and avoid the delay of autocmd approach
+vim.g.copilot_enabled = 0
+-- this will allow me to enable it iven if Tab key is used for comp.nvim
+vim.g.copilot_no_tab_map = 1
+vim.g.copilot_assume_mapped = 1
+vim.g.copilot_tab_fallback = ''
+
+-- workaround for https://github.com/orgs/community/discussions/16800
+vim.g.copilot_node_command = "/home/dtsf/.asdf/installs/nodejs/17.9.1/bin/node"
--- init.lua
+++ init.lua
@@ -2,6 +2,3 @@ require 'dtsf.options'
 require 'dtsf.bootstrap'
 require 'dtsf.plugins'
 require 'dtsf.keymaps'
-
--- workaround for https://github.com/orgs/community/discussions/16800
-vim.g.copilot_node_command = "/home/dtsf/.asdf/installs/nodejs/17.9.1/bin/node"
