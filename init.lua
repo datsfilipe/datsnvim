5e78e8514abe8e7e9d7a2027b72b4d804461ac24
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-07 08:52:52 -0300

    feat(init): set leader key in init.lua -- it's a good thing to set it early in config

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@ if require 'dtsf.first_load'() then
   return
 end
 
+-- leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require 'dtsf.disable_builtins'
 require 'dtsf.highlights'
 require 'dtsf.options'
+require 'dtsf.bindings'
