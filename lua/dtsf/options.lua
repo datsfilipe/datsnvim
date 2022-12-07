d354d207862a1cfd7758681ae728e019145fc058
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-06 22:02:32 -0300

    feat(options): add global options for neovim

---
+++ lua/dtsf/options.lua
@@ -0,0 +1,87 @@
+local opt = vim.opt
+
+-- general
+opt.backspace = { 'start', 'eol', 'indent' }
+opt.inccommand = 'split'
+opt.path:append { '**' } -- finding files - search down into subfolders
+opt.diffopt = { 'internal', 'filler', 'closeoff', 'hiddenoff', 'algorithm:minimal' }
+opt.joinspaces = false
+opt.shell = 'fish'
+opt.cmdheight = 1
+opt.timeoutlen = 600
+opt.updatetime = 1000 -- make updates happen faster
+opt.showcmd = true
+opt.showmode = false
+opt.scrolloff = 10
+
+-- split
+opt.equalalways = false
+opt.splitbelow = true
+opt.splitright = true
+
+-- file
+opt.backup = false
+vim.scriptencoding = 'utf-8'
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+opt.swapfile = false
+opt.undofile = true
+opt.wildignore:append { '*/node_modules/*' }
+opt.wildignore:append { 'Cargo.lock' }
+
+-- search
+opt.incsearch = true
+opt.hlsearch = true
+opt.showmatch = true
+opt.ignorecase = true -- ignore case when searching...
+opt.smartcase = true
+
+-- tabs
+opt.foldmethod = 'marker'
+opt.foldlevel = 0
+opt.modelines = 1
+opt.belloff = 'all'
+opt.shada = { '!', '\'1000', '<50', 's10', 'h' }
+opt.hidden = true
+
+-- indent
+opt.expandtab = true
+opt.wrap = false -- no wrap lines
+opt.showbreak = string.rep(' ', 3) -- Make it so that long lines wrap smartly
+opt.smarttab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+opt.smartindent = true
+opt.breakindent = true
+opt.linebreak = true
+
+-- ui options
+opt.number = true
+opt.relativenumber = true
+opt.title = true
+
+opt.ruler = false
+opt.guicursor = 'a:blinkon5' -- set same cursor to all modes (5 is the blink time)
+opt.laststatus = 3
+opt.fillchars = {
+  vert = '│',
+  eob = ' ',
+  diff = ' ',
+  msgsep = ' '
+}
+
+opt.pumblend = 17
+opt.wildmode = 'longest:full'
+opt.wildoptions = 'pum'
+
+-- undercurl
+vim.cmd([[let &t_Cs = '\e[4:3m']])
+vim.cmd([[let &t_Ce = '\e[4:0m']])
+
+-- disable intro
+opt.shortmess:append 'sI'
+
+-- add asterisks in block comments
+opt.formatoptions:append { 'r' }
