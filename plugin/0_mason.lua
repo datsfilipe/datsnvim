5a73e4cd66c2fb4089a857165d0bbc4ee41f8d6d
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-07 23:24:33 -0300

    feat(plugin/mason): add config

---
+++ plugin/0_mason.lua
@@ -0,0 +1,30 @@
+local ok, mason = pcall(require, 'mason')
+if not ok then
+  return
+end
+
+local ok2, lspconfig = pcall(require, 'mason-lspconfig')
+if not ok2 then
+  return
+end
+
+mason.setup {
+  ui = {
+    icons = {
+      package_installed = ' ',
+      package_pending = '',
+      package_uninstalled = '✗ ',
+    },
+  },
+}
+
+lspconfig.setup {
+  ensure_installed = {
+    'rust_analyzer',
+    'tsserver',
+    'gopls',
+    'tailwindcss',
+    'sumneko_lua',
+    'astro',
+  },
+}
