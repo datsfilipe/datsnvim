da6871297d1b91f4cdc3174ad294d09848864746
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-07 23:19:00 -0300

    fix(plugins): ts update and ts context errors while packer first install and add copilot

--- lua/dtsf/plugins.lua
+++ lua/dtsf/plugins.lua
@@ -1,11 +1,11 @@
 local ok, packer = pcall(require, 'packer')
 if not ok then
-  print('Packer is not installed')
+  print 'Packer is not installed'
   return
 end
 
 -- init options
-packer.init({
+packer.init {
   auto_clean = true,
   compile_on_sync = true,
   git = { clone_timeout = 5000 },
@@ -16,7 +16,7 @@ packer.init({
     removed_sym = ' ',
     moved_sym = '',
   },
-})
+}
 
 packer.startup {
   function(use)
@@ -33,19 +33,21 @@ packer.startup {
     -- ui
     use {
       'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     }
     use 'kyazdani42/nvim-web-devicons'
-    use "yamatsum/nvim-web-nonicons"
+    use 'yamatsum/nvim-web-nonicons'
 
     -- must have telescope
     use {
       'nvim-telescope/telescope.nvim',
-      requires = { 'nvim-lua/plenary.nvim' }
+      requires = { 'nvim-lua/plenary.nvim' },
     }
     use {
       'nvim-telescope/telescope-file-browser.nvim',
-      requires = { 'nvim-telescope/telescope.nvim' }
+      'nvim-telescope/telescope-dap.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+      requires = { 'nvim-telescope/telescope.nvim' },
     }
     -- colorschemes
     use 'ellisonleao/gruvbox.nvim'
@@ -54,22 +56,36 @@ packer.startup {
     -- utilities
     use {
       'ThePrimeagen/harpoon',
-      requires = { 'nvim-lua/plenary.nvim' }
+      requires = { 'nvim-lua/plenary.nvim' },
     }
     use 'dinhhuy258/git.nvim'
-    use({
+    use {
       'iamcco/markdown-preview.nvim',
-      run = function() vim.fn['mkdp#util#install']() end,
-    })
+      run = function()
+        vim.fn['mkdp#util#install']()
+      end,
+    }
     use 'wakatime/vim-wakatime'
     use 'lewis6991/gitsigns.nvim'
 
     -- treesitter
     use {
       'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+      run = function()
+        local ts_update = require('nvim-treesitter.install').update { with_sync = true }
+        ts_update()
+      end,
     }
-    use 'nvim-treesitter/nvim-treesitter-context'
+    use {
+      'nvim-treesitter/nvim-treesitter-context',
+      requires = { 'nvim-treesitter/nvim-treesitter' },
+      after = 'nvim-treesitter',
+    }
+
+    -- debug adapter protocol
+    use 'mfussenegger/nvim-dap'
+    use 'rcarriga/nvim-dap-ui'
+    use 'theHamsta/nvim-dap-virtual-text'
 
     -- lsp
     use 'williamboman/mason.nvim'
@@ -89,12 +105,28 @@ packer.startup {
     use 'saadparwaiz1/cmp_luasnip'
 
     use {
+      'zbirenbaum/copilot.lua',
+      config = function()
+        require('copilot').setup()
+      end,
+    }
+
+    use {
+      'zbirenbaum/copilot-cmp',
+      after = { 'copilot.lua' },
+      config = function()
+        require('copilot_cmp').setup()
+      end,
+    }
+
+    use {
       'windwp/nvim-ts-autotag',
       requires = { 'nvim-treesitter/nvim-treesitter' },
-      after = 'nvim-treesitter'
+      after = 'nvim-treesitter',
     }
     use 'lukas-reineke/indent-blankline.nvim'
     use 'windwp/nvim-autopairs'
+    use 'norcalli/nvim-colorizer.lua'
   end,
 }
 
