67632c0bac1509687a65d5f6a0f0ff6ba461bd11
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-07 23:22:18 -0300

    feat(plugin/lspkind): add config

---
+++ after/plugin/lspkind.lua
@@ -0,0 +1,42 @@
+local ok, lspkind = pcall(require, 'lspkind')
+if not ok then
+  return
+end
+
+lspkind.init {
+  -- default: true
+  mode = 'symbol',
+
+  preset = 'default',
+
+  symbol_map = {
+    Text = '',
+    Method = '',
+    Function = '',
+    Constructor = '',
+    Field = 'ﰠ',
+    Variable = '',
+    Class = 'ﴯ',
+    Interface = '',
+    Module = '',
+    Property = 'ﰠ',
+    Unit = '塞',
+    Value = '',
+    Enum = '',
+    Keyword = '',
+    Snippet = '',
+    Color = '',
+    File = '',
+    Reference = '',
+    Folder = '',
+    EnumMember = '',
+    Constant = '',
+    Struct = 'פּ',
+    Event = '',
+    Operator = '',
+    TypeParameter = '',
+    Copilot = '',
+  },
+}
+
+vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
