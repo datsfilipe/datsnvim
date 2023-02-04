83daddb7b65a4d7f6ba458e3985c59063771a446
datsfilipe <datsfilipe.dev@gmail.com>
2023-02-03 21:04:47 -0300

    feat(packer): prompt question per system session and create a temporary keymap to download packer
    The keymap will only be present if packer isn't installed. Now packer config have a proper
    directory, where I will be moving some other configs later. Also now leader map is set before
    calling packer auto install scripts. More separated files with better organization for packer
    config.

---
+++ lua/dtsf/packer/download.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.download_packer = function()
+  local directory = string.format('%s/site/pack/packer/start/', vim.fn.stdpath 'data')
+
+  vim.fn.mkdir(directory, 'p')
+
+  local out = vim.fn.system(
+    string.format('git clone %s %s', 'https://github.com/wbthomason/packer.nvim', directory .. '/packer.nvim')
+  )
+
+  print(out)
+  print 'Downloading packer.nvim...'
+  vim.cmd [[qa]]
+end
+
+return M
---
+++ lua/dtsf/packer/prompt_question.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+local question_file = '/tmp/.nvim_packer_question'
+
+M.prompt_question = function()
+  if os.rename(question_file, question_file) == nil then
+    os.execute('touch ' .. question_file)
+  else
+    return false
+  end
+
+  if vim.fn.input('Download Packer? (y for yes)') ~= 'y' then
+    return false
+  end
+
+  return true
+end
+
+return M
