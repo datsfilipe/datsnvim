92a36d1876c186f77bbfa41068f8ab1e77d0ba47
datsfilipe <datsfilipe.pro@proton.me>
2023-03-06 08:44:07 -0300

    feat(gruvbox): add gruvbox config

---
+++ lua/dtsf/plugins/configs/gruvbox.lua
@@ -0,0 +1,29 @@
+local present, gruvbox = pcall(require, 'gruvbox')
+if not present then
+  return
+end
+
+gruvbox.setup {
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = false,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true,
+  contrast = 'hard',
+  palette_overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+  overrides = {
+    -- change telescope window highlight
+    TelescopeResultsBorder = { fg = '#504945', bg = 'NONE' },
+    TelescopePromptBorder = { fg = '#504945', bg = 'NONE' },
+    TelescopePreviewBorder = { fg = '#504945', bg = 'NONE' },
+  },
+}
+
+vim.cmd 'colorscheme gruvbox'
