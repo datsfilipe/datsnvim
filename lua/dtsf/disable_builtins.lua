64322074379bc661a609a9ab64f4fbd47d256b2d
datsfilipe <datsfilipe.pro@proton.me>
2023-04-03 22:43:47 -0300

    refactor: enable netrw

--- lua/dtsf/disable_builtins.lua
+++ lua/dtsf/disable_builtins.lua
@@ -15,6 +15,4 @@ vim.g.loaded_matchparen = 1
 vim.g.loaded_logiPat = 1
 vim.g.loaded_rrhelper = 1
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
+vim.g.netrw_banner = 0
