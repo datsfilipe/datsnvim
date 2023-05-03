335a0af70e241c15464fec0cee4f3e4f7241204d
datsfilipe <datsfilipe.foss@proton.me>
2023-05-03 14:20:39 -0300

    feat: add github-theme as colorscheme

---
+++ lua/dtsf/plugins/configs/github-theme.lua
@@ -0,0 +1,13 @@
+local present, github = pcall(require, "github-theme")
+if not present then
+  return
+end
+
+github.setup({
+  transparent = true,
+})
+
+vim.cmd('colorscheme github_dark')
+
+vim.cmd('hi IndentBlanklineIndent1 guifg=#2a2e36')
+vim.cmd('hi LineNr guifg=#3e4452')
