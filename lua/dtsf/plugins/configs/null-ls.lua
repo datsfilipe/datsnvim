d8149ca7aaec553870ffc33120c8bca3225a0969
datsfilipe <datsfilipe.foss@proton.me>
2023-05-18 11:19:00 -0300

    chore: remove rustywind integration in null-ls

--- lua/dtsf/plugins/configs/null-ls.lua
+++ lua/dtsf/plugins/configs/null-ls.lua
@@ -26,7 +26,6 @@ local sources = {
   -- formatting
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.stylua,
-  null_ls.builtins.formatting.rustywind,
   -- code actions
   null_ls.builtins.code_actions.gitsigns,
 }
