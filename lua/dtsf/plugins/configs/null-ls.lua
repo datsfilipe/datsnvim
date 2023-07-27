349f8a575ecfd508bd59e3df7004f1c44c061435
datsfilipe <datsfilipe.foss@proton.me>
2023-07-27 06:50:03 -0300

    refactor(null-ls): left only basic sources
    * remove not often used null-ls sources
    
    Will keep null-ls for now, waiting hopefully for a new maintainer or a replacement.

--- lua/dtsf/plugins/configs/null-ls.lua
+++ lua/dtsf/plugins/configs/null-ls.lua
@@ -19,9 +19,6 @@ local sources = {
   null_ls.builtins.diagnostics.eslint_d.with {
     diagnostics_format = '[#{c}] #{m} (#{s})',
   },
-  null_ls.builtins.diagnostics.commitlint, -- git commit messages
-  null_ls.builtins.diagnostics.proselint, -- prose linter
-  null_ls.builtins.diagnostics.editorconfig_checker, -- editorconfig
   -- formatting
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.stylua,
