41a9e913bc56456f65580eecaf7ac0fe8a6d4df1
datsfilipe <datsfilipe.foss@proton.me>
2023-07-27 05:00:09 -0300

    fix(lualine): wrong theme being applied

--- lua/dtsf/plugins/configs/lualine.lua
+++ lua/dtsf/plugins/configs/lualine.lua
@@ -6,7 +6,7 @@ end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'codedark',
     component_separators = { left = '|', right = '|' },
     section_separators = { left = '', right = '' },
   },
