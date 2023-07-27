97f49f794ea2bc243ac135ac3b0edeebb3b0c2c9
datsfilipe <datsfilipe.foss@proton.me>
2023-07-27 05:01:55 -0300

    refactor(plugins): remove telescope file browser comment

--- lua/dtsf/plugins/init.lua
+++ lua/dtsf/plugins/init.lua
@@ -59,7 +59,6 @@ return {
     end,
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- 'nvim-telescope/telescope-file-browser.nvim',
       'nvim-telescope/telescope-dap.nvim',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
