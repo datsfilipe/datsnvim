7783cdec89034eb1109d385f2981ef0e2c4cd1ce
datsfilipe <datsfilipe.foss@proton.me>
2023-09-04 20:21:57 -0300

    chore: change theme to min-theme.nvim

--- lua/dtsf/plugins/configs/lualine.lua
+++ lua/dtsf/plugins/configs/lualine.lua
@@ -6,7 +6,7 @@ end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'codedark',
+    theme = 'min-theme',
     component_separators = { left = '|', right = '|' },
     section_separators = { left = '', right = '' },
   },
