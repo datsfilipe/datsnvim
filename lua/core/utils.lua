8acbcba4d443c7c2dba0ae7c2ce22bcb340de7f1
datsfilipe <datsfilipe.foss@proton.me>
2023-09-29 18:31:57 -0300

    feat: add gh cli integration for creating PRs

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -11,4 +11,42 @@ M.diagnostic_symbols = {
   hint = "󰌶",
 }
 
+-- gh
+local function return_project_root()
+  local path = vim.fn.expand "%:p:h"
+  local git_path = vim.fn.finddir(".git", path .. ";")
+  if git_path == "" then
+    return nil
+  else
+    return vim.fn.fnamemodify(git_path, ":h")
+  end
+end
+
+M.create_pr = function()
+  local template_path = return_project_root() .. "/.github/pull_request_template.md"
+
+  vim.cmd("tabe " .. vim.fn.tempname())
+
+  if vim.fn.filereadable(template_path) == 1 then
+    local template_lines = vim.fn.readfile(template_path)
+    vim.fn.setline(1, template_lines)
+  end
+
+  vim.cmd "setlocal filetype=markdown"
+  vim.cmd "setlocal buftype=nofile"
+  vim.cmd "setlocal bufhidden=delete"
+end
+
+M.submit_pr = function()
+  local branch = "main"
+  local title = vim.fn.getline(1)
+  local body = vim.fn.join(vim.fn.getline(3, "$"), "\n")
+
+  local cmd = string.format("gh pr -a @me create --base %s --title '%s' --body '%s'", branch, title, body)
+
+  local _, result = pcall(vim.fn.system, cmd)
+  print(result)
+  vim.cmd "bd!"
+end
+
 return M
--- lua/keymap/integrations.lua
+++ lua/keymap/integrations.lua
@@ -4,3 +4,7 @@ local opts = { noremap = true, silent = true }
 
 map { "n", "<leader>x", "<cmd>!chmod +x %<CR>", opts } -- make file executable
 map { "n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>" }
+
+-- gh
+map { "n", "<leader>pr", "<cmd>lua require('core.utils').create_pr()<CR>", opts }
+map { "n", "<leader>ps", "<cmd>lua require('core.utils').submit_pr()<CR>", opts }
