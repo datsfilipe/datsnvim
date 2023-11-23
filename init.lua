950760ddb6d4bed6fc0b05dc08f42d615f1813be
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 16:38:50 -0300

    refactor: update config require and call colorscheme in init.lua

--- init.lua
+++ init.lua
@@ -2,4 +2,7 @@ require "core.unset"
 require "core.options"
 require "core.autocmds"
 require "core.keymaps"
-require "plugins.lazy"
+require "utils.lazy"
+
+local colorscheme = require "utils.config".colorscheme
+vim.cmd.colorscheme(colorscheme)
