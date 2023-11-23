25d0d6ff0f5b97866b81a52e589a79ef6bfefddb
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 12:56:52 -0300

    fix: netrw maps asking for Enter to confirm

--- lua/core/keymaps/editor.lua
+++ lua/core/keymaps/editor.lua
@@ -17,10 +17,10 @@ keymap.set("v", "g+", "g<C-a>", opts)
 keymap.set("v", "g-", "g<C-x>", opts)
 
 -- move lines
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
-keymap.set("n", "<leader>j", ":m .+1<CR>==", opts)
-keymap.set("n", "<leader>k", ":m .-2<CR>==", opts)
+keymap.set("v", "J", ":m '>+1<Return>gv=gv", opts)
+keymap.set("v", "K", ":m '<-2<Return>gv=gv", opts)
+keymap.set("n", "<leader>j", ":m .+1<Return>==", opts)
+keymap.set("n", "<leader>k", ":m .-2<Return>==", opts)
 
 -- go back to last edited line
 keymap.set("n", "g;", "`[", opts)
@@ -31,25 +31,25 @@ keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
 -- new tab
 keymap.set("n", "te", ":tabedit", opts)
-keymap.set("n", ">", ":tabnext<CR>", opts)
-keymap.set("n", "<", ":tabprev<CR>", opts)
+keymap.set("n", ">", ":tabnext<Return>", opts)
+keymap.set("n", "<", ":tabprev<Return>", opts)
 -- split window
-keymap.set("n", "<leader>]", ":split<CR>", opts)
-keymap.set("n", "<leader>[", ":vsplit<CR>", opts)
+keymap.set("n", "<leader>]", ":split<Return>", opts)
+keymap.set("n", "<leader>[", ":vsplit<Return>", opts)
 -- maximize window
 keymap.set("n", "<leader>-", "<C-w>_<C-w><Bar>", opts)
 -- equalize windows
 keymap.set("n", "<leader>=", "<C-w>=", opts)
 
 -- wrap lines
-keymap.set("n", "<A-z>", ":set wrap!<CR>", opts)
+keymap.set("n", "<A-z>", ":set wrap!<Return>", opts)
 -- indent lines
 keymap.set("v", "<", "<gv", opts)
 keymap.set("v", ">", ">gv", opts)
 
 -- netrw
-keymap.set("n", "<leader>e", ":Explore<CR>", opts)
-keymap.set("n", "<leader>s", ":Sexplore<CR>", opts)
+keymap.set("n", "<leader>e", ":Explore<Return><Return>", opts)
+keymap.set("n", "<leader>s", ":Sex<Return><Return>", opts)
 
 -- replace occurences
 keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
@@ -75,7 +75,7 @@ keymap.set("n", "<C-k>", function()
 end, opts)
 
 -- make file executable
-keymap.set("n", "<leader>x", ":!chmod +x %<CR>", opts)
+keymap.set("n", "<leader>x", ":!chmod +x %<Return>", opts)
 
 -- tmux-sessionizer (https://github.com/datsfilipe/unix-scripts/blob/main/tmux-sessionizer)
-keymap.set("n", "<C-f>", ":silent !tmux neww tmux-sessionizer<CR>", opts)
+keymap.set("n", "<C-f>", ":silent !tmux neww tmux-sessionizer<Return>", opts)
