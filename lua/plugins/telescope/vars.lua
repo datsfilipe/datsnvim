7b0c008382ae7e074171fd70239a9b34fa117ce8
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 00:13:46 -0300

    feat: add telescope

---
+++ lua/plugins/telescope/vars.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.actions = require "telescope.actions"
+M.action_state = require "telescope.actions.state"
+M.action_layout = require "telescope.actions.layout"
+M.builtin = require "telescope.builtin"
+
+return M
