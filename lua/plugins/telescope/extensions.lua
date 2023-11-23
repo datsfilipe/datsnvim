4c6c8ad82cc10b659a530230b3f30c50eb259aca
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 00:13:46 -0300

    feat: add telescope

---
+++ lua/plugins/telescope/extensions.lua
@@ -0,0 +1,8 @@
+return {
+  fzf = {
+    fuzzy = true,
+    override_generic_sorter = true,
+    override_file_sorter = true,
+    case_mode = "smart_case",
+  },
+}
---
+++ lua/plugins/telescope/init.lua
@@ -0,0 +1,41 @@
+local ok, telescope = pcall(require, "telescope")
+if not ok then
+  return
+end
+
+local maps = require "core.keymaps.integrations.telescope"
+
+local actions = require("plugins.telescope.vars").actions
+local action_state = require("plugins.telescope.vars").action_state
+local action_layout = require("plugins.telescope.vars").action_layout
+local extensions = require("plugins.telescope.extensions").extensions
+local builtin = require("plugins.telescope.vars").builtin
+
+local set_prompt_to_entry_value = function(prompt_bufnr)
+  local entry = action_state.get_selected_entry()
+  if not entry or not type(entry) == "table" then
+    return
+  end
+
+  action_state.get_current_picker(prompt_bufnr):reset_prompt(entry.ordinal)
+end
+
+telescope.setup {
+  defaults = {
+    prompt_prefix = "   ",
+    selection_caret = "  ",
+    entry_prefix = "  ",
+    border = {},
+    borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+    -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    layout_strategy = "flex",
+    sorting_strategy = "ascending",
+    mappings = maps.inner_maps(actions, action_layout, set_prompt_to_entry_value),
+  },
+  extensions = extensions,
+}
+
+telescope.load_extension "harpoon"
+telescope.load_extension "fzf"
+
+maps.outer_maps(telescope.extensions, builtin)
