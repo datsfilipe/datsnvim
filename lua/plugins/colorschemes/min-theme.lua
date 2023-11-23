f89873a13b05111064b8562775ffa66d23520994
datsfilipe <datsfilipe.foss@proton.me>
2023-11-23 00:13:13 -0300

    feat: add colorschemes

---
+++ lua/plugins/colorschemes/min-theme.lua
@@ -0,0 +1,25 @@
+local ok, min = pcall(require, "min-theme")
+if not ok then
+  return
+end
+
+min.setup {
+  theme = "dark",
+  transparent = true,
+  italics = {
+    comments = false,
+    keywords = false,
+    functions = false,
+    strings = false,
+    variables = false,
+  },
+  overrides = {
+    IndentBlanklineIndent1 = { fg = "#282828", bg = "NONE" },
+    NormalFloat = { bg = "NONE" },
+  },
+}
+
+vim.cmd [[colorscheme min-theme]]
+
+vim.cmd [[highlight CmpBorder guifg=#282828]]
+vim.cmd [[highlight CmpDocBorder guifg=#282828]]
