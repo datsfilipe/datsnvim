9c7df465d2eb5ea443f757dabec22554e9381327
datsfilipe <datsfilipe.foss@proton.me>
2023-11-24 14:44:19 -0300

    refactor(mason): add elixirls and remove lua-ls setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,17 +14,14 @@ return {
         ensure_installed = {
           "lua_ls",
           "gopls",
+          "elixirls",
+          "tsserver",
           "marksman",
           "rust_analyzer",
           "tailwindcss",
-          "tsserver",
         },
         handlers = {
           lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
         }
       })
     end,
