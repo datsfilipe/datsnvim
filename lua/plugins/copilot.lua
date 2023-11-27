96363bd49f6edf47ff38ad5a9baedaf60da12801
datsfilipe <datsfilipe.foss@proton.me>
2023-11-27 18:27:23 -0300

    feat: add copilot keymaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,11 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "<leader>cp", ":Copilot panel<Return>", opts)
+keymap.set("n", "<leader>ce", ":Copilot enable<Return>", opts)
+keymap.set("n", "<leader>cd", ":Copilot disable<Return>", opts)
+keymap.set("n", "<leader>cs", ":Copilot status<Return>", opts)
+
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
