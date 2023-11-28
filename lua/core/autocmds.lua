ad990745842a6ea7d07ae1938945102b2a7dcc75
datsfilipe <datsfilipe.foss@proton.me>
2023-11-27 22:32:38 -0300

    feat: new custom script for monkeytype training

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,15 +1,17 @@
+require "custom.monkeytype".init(true) -- monkeytype training: on
+
 -- turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
 
 -- disable the concealing in some file formats
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "json", "jsonc", "markdown" },
-	callback = function()
-		vim.opt.conceallevel = 0
-	end,
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
 })
 
 -- set filetype of .astro files
