e572cecc1c75c2652045276a307d7bfeed9159d8
datsfilipe <datsfilipe.foss@proton.me>
2023-12-01 11:19:15 -0300

    refactor(none-ls): update format on save to async format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,6 +1,37 @@
 local config = require "utils.config"
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local async_formatting = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  vim.lsp.buf_request(
+    bufnr,
+    "textDocument/formatting",
+    vim.lsp.util.make_formatting_params({}),
+    function(err, res, ctx)
+      if err then
+        local err_msg = type(err) == "string" and err or err.message
+        -- you can modify the log message / level (or ignore it completely)
+        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+        return
+      end
+
+      -- don't apply results if buffer is unloaded or has been modified
+      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+        return
+      end
+
+      if res then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("silent noautocmd update")
+        end)
+      end
+    end
+  )
+end
+
 return {
   "nvimtools/none-ls.nvim",
   event = "BufReadPre",
@@ -15,16 +46,17 @@ return {
       sources = {
         -- formatting.prettier,
         formatting.eslint_d,
+        formatting.rustywind,
         diagnostics.flake8,
       },
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
           vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
+          vim.api.nvim_create_autocmd("BufWritePost", {
             group = augroup,
             buffer = bufnr,
             callback = function()
-              vim.lsp.buf.format({ async = false })
+              async_formatting(bufnr)
             end,
           })
         end
