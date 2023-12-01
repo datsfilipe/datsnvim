36463fa96882fbdcd8888a67a3c7058fde22d69f
datsfilipe <datsfilipe.foss@proton.me>
2023-12-01 04:54:03 -0300

    refactor: get rid of lsp-zero
    closes #14

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,76 +1,145 @@
+local keymap = vim.keymap
+local config = require "utils.config"
+
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    event = "BufEnter",
-    config = function()
-      local lsp_zero = require "lsp-zero"
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "folke/neodev.nvim",
+  },
+  event = "BufReadPre",
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-      local keymap = vim.keymap
+    -- Setting up on_attach
+    local on_attach = function(client, bufnr)
+      local opts = { silent = true, buffer = bufnr }
 
-      lsp_zero.extend_lspconfig()
-      lsp_zero.on_attach(function(_, bufnr)
-        local opts = { buffer = bufnr, remap = false }
+      keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+      end, opts)
 
-        keymap.set("n", "gd", function()
-          vim.lsp.buf.definition()
-        end, opts)
+      keymap.set("n", "<leader>vws", function()
+        vim.lsp.buf.workspace_symbol()
+      end, opts)
 
-        keymap.set("n", "<leader>vws", function()
-          vim.lsp.buf.workspace_symbol()
-        end, opts)
+      keymap.set("n", "<leader>vd", function()
+        vim.diagnostic.open_float()
+      end, opts)
 
-        keymap.set("n", "<leader>vd", function()
-          vim.diagnostic.open_float()
-        end, opts)
+      keymap.set("n", "<leader>n", function()
+        vim.diagnostic.goto_next()
+      end, opts)
 
-        keymap.set("n", "<leader>n", function()
-          vim.diagnostic.goto_next()
-        end, opts)
+      keymap.set("n", "<leader>p", function()
+        vim.diagnostic.goto_prev()
+      end, opts)
 
-        keymap.set("n", "<leader>p", function()
-          vim.diagnostic.goto_prev()
-        end, opts)
+      keymap.set("n", "<leader>vca", function()
+        vim.lsp.buf.code_action()
+      end, opts)
 
-        keymap.set("n", "<leader>vca", function()
-          vim.lsp.buf.code_action()
-        end, opts)
+      keymap.set("n", "<leader>vrr", function()
+        vim.lsp.buf.references()
+      end, opts)
 
-        keymap.set("n", "<leader>vrr", function()
-          vim.lsp.buf.references()
-        end, opts)
+      keymap.set("n", "<leader>vrn", function()
+        vim.lsp.buf.rename()
+      end, opts)
 
-        keymap.set("n", "<leader>vrn", function()
-          vim.lsp.buf.rename()
-        end, opts)
+      keymap.set("i", "<C-h>", function()
+        vim.lsp.buf.signature_help()
+      end, opts)
 
-        keymap.set("i", "<C-h>", function()
-          vim.lsp.buf.signature_help()
-        end, opts)
+      keymap.set("n", "K", function()
+        vim.lsp.buf.hover()
+      end, opts)
 
-        keymap.set("n", "K", function()
-          vim.lsp.buf.hover()
-        end, opts)
-      end)
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+      keymap.set("n", "gD", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+      keymap.set("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts)
+      keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+      keymap.set("n", "<leader>ld", "<cmd>Telescope diagnostics<cr>", opts)
 
-      -- diagnostics
-      vim.diagnostic.config {
-        virtual_text = {
-          prefix = "●",
-          spacing = 0,
-        },
+      -- tsserver specific
+      if client.name == "tsserver" then
+        client.server_capabilities.documentFormattingProvider = false
+      end
+    end
+
+    -- setup servers
+    for _, server in pairs(config.servers) do
+      Opts = {
+        on_attach = on_attach,
+        capabilities = capabilities,
       }
 
-      vim.cmd [[
-        sign define DiagnosticSignError text=● texthl=DiagnosticSignError linehl= numhl=
-        sign define DiagnosticSignWarn text=● texthl=DiagnosticSignWarn linehl= numhl=
-        sign define DiagnosticSignInfo text=● texthl=DiagnosticSignInfo linehl= numhl=
-        sign define DiagnosticSignHint text=● texthl=DiagnosticSignHint linehl= numhl=
-      ]]
+      server = vim.split(server, "@")[1]
+      lspconfig[server].setup(Opts)
     end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-  },
+
+    -- setup up border for LspInfo
+    require("lspconfig.ui.windows").default_options.border = "rounded"
+
+    -- setup icons for diagnostics
+    local signs = {
+      { name = "DiagnosticSignError", text = config.signs.Error },
+      { name = "DiagnosticSignWarn",  text = config.signs.Warn },
+      { name = "DiagnosticSignHint",  text = config.signs.Hint },
+      { name = "DiagnosticSignInfo",  text = config.signs.Info },
+    }
+    for _, sign in ipairs(signs) do
+      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
+
+    vim.diagnostic.config({
+      virtual_text = true,
+      signs = {
+        active = signs,
+      },
+      update_in_insert = true,
+      underline = true,
+      severity_sort = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+        suffix = "",
+      },
+    })
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    })
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = "rounded",
+    })
+    -- Setting up lua server
+    lspconfig.lua_ls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    })
+  end,
 }
