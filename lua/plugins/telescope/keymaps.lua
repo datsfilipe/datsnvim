0e710c5b2580207dc60885452f363a6fabd4a0f9
datsfilipe <datsfilipe.foss@proton.me>
2023-12-05 10:03:18 -0300

    refactor: update telescope keymaps with theme

--- lua/plugins/telescope/keymaps.lua
+++ lua/plugins/telescope/keymaps.lua
@@ -2,8 +2,9 @@ local M = {}
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local theme = require "plugins.telescope.vars".theme
 
-M.inner_maps = function(actions, action_layout, set_prompt_to_entry_value)
+M.inner_maps = function(actions, _, set_prompt_to_entry_value)
   return {
     i = {
       ["<C-x>"] = false,
@@ -15,10 +16,9 @@ M.inner_maps = function(actions, action_layout, set_prompt_to_entry_value)
       ["<C-d>"] = actions.results_scrolling_down,
       ["<C-u>"] = actions.results_scrolling_up,
       ["<C-l>"] = set_prompt_to_entry_value,
-      ["<C-p>"] = action_layout.toggle_preview,
       ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+      ["<C-a>"] = actions.add_selection,
       ["<C-g>a"] = actions.select_all,
-      ["<C-g>s"] = actions.add_selection,
     },
     n = {
       ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
@@ -30,47 +30,47 @@ end
 
 M.outer_maps = function(extensions, builtin)
   keymap.set("n", ";h", function()
-    extensions.harpoon.marks {
+    extensions.harpoon.marks(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
-    }
+    }, theme))
   end, opts)
 
   keymap.set("n", ";f", function()
-    builtin.find_files {
+    builtin.find_files(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
       no_ignore = false,
       hidden = true,
-    }
+    }, theme))
   end, opts)
 
   keymap.set("n", ";r", function()
-    builtin.live_grep {
+    builtin.live_grep(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
-    }
+    }, theme))
   end, opts)
 
   keymap.set("n", "\\\\", function()
-    builtin.buffers {
+    builtin.buffers(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
-    }
+    }, theme))
   end, opts)
 
   keymap.set("n", ";t", function()
-    builtin.help_tags {
+    builtin.help_tags(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
-    }
+    }, theme))
   end, opts)
 
   keymap.set("n", ";e", function()
-    builtin.diagnostics {
+    builtin.diagnostics(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
-    }
+    }, theme))
   end, opts)
 
   keymap.set("n", ";k", function()
-    builtin.keymaps {
+    builtin.keymaps(vim.tbl_deep_extend("force", {
       prompt_prefix = "   ",
-    }
+    }, theme))
   end, opts)
 end
 
