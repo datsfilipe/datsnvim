306acee003afd9eff56bee9b31f942be7c47864b
datsfilipe <datsfilipe.foss@proton.me>
2023-12-05 10:03:30 -0300

    feat: set telescope theme

--- lua/plugins/telescope/vars.lua
+++ lua/plugins/telescope/vars.lua
@@ -4,5 +4,6 @@ M.actions = require "telescope.actions"
 M.action_state = require "telescope.actions.state"
 M.action_layout = require "telescope.actions.layout"
 M.builtin = require "telescope.builtin"
+M.theme = require "telescope.themes".get_dropdown {}
 
 return M
