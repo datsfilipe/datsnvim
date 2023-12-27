a727f26d7d33be9390d738ab9997f354b15d4dcd
datsfilipe <datsfilipe.foss@proton.me>
2023-12-27 10:21:08 -0300

    feat: highlight hsl colors

---
+++ lua/utils/hsl.lua
@@ -0,0 +1,50 @@
+local M = {}
+
+function M.hslToRgb(h, s, l)
+  local r, g, b
+
+  if s == 0 then
+    r, g, b = l, l, l -- achromatic
+  else
+    local function hue2rgb(p, q, t)
+      if t < 0 then
+        t = t + 1
+      end
+      if t > 1 then
+        t = t - 1
+      end
+      if t < 1 / 6 then
+        return p + (q - p) * 6 * t
+      end
+      if t < 1 / 2 then
+        return q
+      end
+      if t < 2 / 3 then
+        return p + (q - p) * (2 / 3 - t) * 6
+      end
+      return p
+    end
+
+    local q
+    if l < 0.5 then
+      q = l * (1 + s)
+    else
+      q = l + s - l * s
+    end
+    local p = 2 * l - q
+
+    r = hue2rgb(p, q, h + 1 / 3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1 / 3)
+  end
+
+  return r * 255, g * 255, b * 255
+end
+
+function M.hslToHex(h, s, l)
+  local r, g, b = M.hslToRgb(h / 360, s / 100, l / 100)
+
+  return string.format('#%02x%02x%02x', r, g, b)
+end
+
+return M
