880e16fb2f5fa2f3929026af36d3faee9752f481
datsfilipe <datsfilipe.foss@proton.me>
2023-12-27 10:21:08 -0300

    feat: highlight hsl colors

--- lua/plugins/hipatterns.lua
+++ lua/plugins/hipatterns.lua
@@ -1,13 +1,23 @@
 return {
-  "echasnovski/mini.hipatterns",
+  'echasnovski/mini.hipatterns',
   version = false,
-  event = "VeryLazy",
+  event = 'VeryLazy',
   config = function()
-    local hipatterns = require "mini.hipatterns"
+    local hipatterns = require 'mini.hipatterns'
+    local hsl = require 'utils.hsl'
 
     hipatterns.setup {
       highlighters = {
         hex_color = hipatterns.gen_highlighter.hex_color(),
+        hsl_color = {
+          pattern = 'hsl%(%d+,? %d+,? %d+%)',
+          group = function(_, match)
+            local h, s, l = match:match 'hsl%((%d+),? (%d+),? (%d+)%)'
+            h, s, l = tonumber(h), tonumber(s), tonumber(l)
+            local hex_color = hsl.hslToHex(h, s, l)
+            return hipatterns.compute_hex_color_group(hex_color, 'bg')
+          end,
+        },
       },
     }
   end,
