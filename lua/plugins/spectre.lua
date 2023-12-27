258467d1a2171ef9c52995d779cd23fb33db2459
datsfilipe <datsfilipe.foss@proton.me>
2023-12-27 10:19:27 -0300

    chore: remove some plugins

--- lua/plugins/spectre.lua
+++
@@ -1,9 +0,0 @@
-local keymap = vim.keymap
-
-keymap.set("n", "<leader>S", ":lua require('spectre').toggle()<CR>", { noremap = true, silent = true })
-
-return {
-  "nvim-pack/nvim-spectre",
-  event = "VeryLazy",
-  opts = {},
-}
