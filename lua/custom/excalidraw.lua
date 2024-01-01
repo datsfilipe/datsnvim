4f904ef39dc2770ecc63fc5afc5a70c8eec9e5fb
datsfilipe <datsfilipe.foss@proton.me>
2024-01-01 18:53:36 -0300

    feat: add monkeytype custom keybind

---
+++ lua/custom/excalidraw.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.open()
+  vim.fn.system 'xdg-open https://excalidraw.com'
+
+  local clipboard_command
+  if vim.fn.executable 'xclip' == 1 then
+    clipboard_command = 'xclip -selection clipboard'
+  elseif vim.fn.executable 'wl-copy' == 1 then
+    clipboard_command = 'wl-copy'
+  else
+    print 'Error: Neither xclip nor wl-copy is available for clipboard copying.'
+    return
+  end
+
+  vim.fn.system('echo ' .. vim.fn.expand('%:p:h') .. ' | ' .. clipboard_command)
+end
+
+return M
