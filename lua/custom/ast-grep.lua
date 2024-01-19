dd54e2bb362d4651b8c9c629e381b2250f85073e
datsfilipe <datsfilipe.foss@proton.me>
2024-01-19 11:23:36 -0300

    feat: add ast-grep integration + keymap

---
+++ lua/custom/ast-grep.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+local function escape_for_tmux(command)
+  return string.gsub(command, "'", "'\\''")
+end
+
+M.execute = function()
+  local cmd = 'ast-grep ' .. vim.fn.input 'cmd: '
+  local escaped_cmd = escape_for_tmux(cmd)
+  local tmux_cmd = string.format("tmux split-window -h bash -c '%s; cat'", escaped_cmd)
+  vim.fn.system(tmux_cmd)
+end
+
+return M
