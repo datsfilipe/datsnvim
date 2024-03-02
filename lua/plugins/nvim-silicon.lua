988623763a3dc6522acc3ec5b4ec411c1b7c2bb5
datsfilipe <datsfilipe.foss@proton.me>
2024-03-02 00:28:14 -0300

    chore: add nvim-silicon plugin

---
+++ lua/plugins/nvim-silicon.lua
@@ -0,0 +1,21 @@
+return {
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+  opts = {
+    font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+    no_window_controls = true,
+    pad_horiz = 125,
+    pad_vert = 125,
+    background = '#fff',
+    tab_width = 2,
+    theme = os.getenv('HOME') .. '/.config/bat/themes/dtsf-machine/theme.tmTheme',
+    to_clipboard = true,
+    gobble = true,
+    num_separator = "\u{258f} ",
+    shadow_blur_radius = 16,
+    shadow_offset_x = 8,
+    shadow_offset_y = 8,
+    shadow_color = '#000',
+  },
+}
