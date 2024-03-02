19c99e46b09fa9d92996eaa16fcc819474767572
datsfilipe <datsfilipe.foss@proton.me>
2024-03-02 00:25:29 -0300

    chore: add noice.nvim again

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
+    },
+    lsp = {
+      signature = { enabled = false },
+      hover = { enabled = false },
+    },
+  },
+  dependencies = { "MunifTanjim/nui.nvim" },
+}
