be92cf8d37662818d17bed2a7dc84fc49777771e
datsfilipe <datsfilipe.foss@proton.me>
2024-06-09 12:14:51 -0300

    fix: add build arg to md-preview

--- lua/custom/plugins/ui/md-preview.lua
+++ lua/custom/plugins/ui/md-preview.lua
@@ -2,6 +2,9 @@ return {
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = 'markdown',
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
   keys = {
     { '<F12>', '<cmd>MarkdownPreviewToggle<CR>' },
   },
