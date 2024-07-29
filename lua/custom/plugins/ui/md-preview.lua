54cbd03c082130740532ad276848fb9f8598142b
datsfilipe <datsfilipe.foss@proton.me>
2024-07-28 21:55:02 -0300

    fix: using node to build md-preview (only way aaaaa)

--- lua/custom/plugins/ui/md-preview.lua
+++ lua/custom/plugins/ui/md-preview.lua
@@ -2,9 +2,7 @@ return {
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   ft = 'markdown',
-  build = function()
-    vim.fn['mkdp#util#install']()
-  end,
+  build = 'cd app && yarn install',
   keys = {
     { '<F12>', '<cmd>MarkdownPreviewToggle<CR>' },
   },
