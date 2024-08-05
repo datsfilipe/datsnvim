037d7c2e5260593f8f7e7274e89be1bea656ef46
datsfilipe <datsfilipe.foss@proton.me>
2024-08-05 12:37:31 -0300

    feat: add my md previewer to the plugins

--- lua/custom/plugins/ui/md-preview.lua
+++ lua/custom/plugins/ui/md-preview.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  'datsfilipe/md-previewer',
+  cmd = 'MdPreviewer',
+  ft = 'markdown',
+  build = 'bun install && bun compile',
+  keys = {
+    { '<F12>', '<cmd>MdPreviewer<cr>' },
+  },
+  opts = {
+    quiet = true,
+  },
+}
