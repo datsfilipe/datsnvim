09b652e78cbea119a8a84d2d039e1ac0b8259baf
datsfilipe <datsfilipe.foss@proton.me>
2024-09-05 20:37:18 -0300

    chore: remove workspace diagnostics

--- lua/custom/plugins/ui/trouble.lua
+++ lua/custom/plugins/ui/trouble.lua
@@ -1,26 +1,7 @@
 return {
   'folke/trouble.nvim',
-  dependencies = {
-    'artemave/workspace-diagnostics.nvim',
-  },
   keys = {
-    {
-      ';e',
-      function()
-        if not vim.g.workspace_diagnostics_populated then
-          for _, client in ipairs(vim.lsp.get_clients()) do
-            if client.attached_buffers[vim.api.nvim_get_current_buf()] then
-              require('workspace-diagnostics').populate_workspace_diagnostics(
-                client,
-                0
-              )
-            end
-          end
-          vim.g.workspace_diagnostics_populated = true
-        end
-        vim.cmd [[Trouble diagnostics toggle]]
-      end,
-    },
+    { ';e', '<cmd>Trouble diagnostics toggle<Return>' },
     { ';b', '<cmd>Trouble diagnostics toggle filter.buf=0<Return>' },
     { ']d', '<cmd>Trouble diagnostics next<Return>' },
     { '[d', '<cmd>Trouble diagnostics prev<Return>' },
