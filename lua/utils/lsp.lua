762bcb372d2c90ccbfe274abd3e65baa3df294d9
datsfilipe <datsfilipe.foss@proton.me>
2024-11-10 15:50:46 -0300

    fix: add lsp utils and `disabled` field to avoid mason install errors

---
+++ lua/utils/lsp.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.check_for_bin = function(bin)
+  local path = vim.fn.executable(bin)
+  return path == 1
+end
+
+M.filter_by_disabled = function(configs)
+  return vim.tbl_filter(function(key)
+    local t = configs[key]
+    if type(t) == 'table' then
+      return t.disabled ~= true
+    else
+      return t
+    end
+  end, vim.tbl_keys(configs))
+end
+
+return M
