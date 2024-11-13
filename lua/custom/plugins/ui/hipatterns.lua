82ea6c8e6eede6aac2342e2c00c222e87cfceda7
datsfilipe <datsfilipe.foss@proton.me>
2024-11-13 19:35:01 -0300

    refactor: update hipatterns config and add `rgb`/`rgba` support

--- lua/custom/plugins/ui/hipatterns.lua
+++ lua/custom/plugins/ui/hipatterns.lua
@@ -5,6 +5,44 @@ return {
     local hipatterns = require 'mini.hipatterns'
     local color_utils = require 'utils.color_utils'
 
+    local function create_color_highlighter(pattern, extract_fn)
+      return {
+        pattern = pattern,
+        group = function(_, match)
+          local hex_color = extract_fn(match)
+          if hex_color:len() > 7 then
+            hex_color = hex_color:sub(1, 7)
+          end
+          return hipatterns.compute_hex_color_group(hex_color, 'bg')
+        end,
+      }
+    end
+
+    local function extract_hsl(match)
+      local h, s, l = match:match 'hsl%((%d+),? (%d+),? (%d+)%)'
+      return color_utils.hslToHex(tonumber(h), tonumber(s), tonumber(l))
+    end
+
+    local function extract_rgb(match)
+      local r, g, b = match:match 'rgb%((%d+),? (%d+),? (%d+)%)'
+      return string.format(
+        '#%02x%02x%02x',
+        tonumber(r),
+        tonumber(g),
+        tonumber(b)
+      )
+    end
+
+    local function extract_rgba(match)
+      local r, g, b = match:match 'rgba%((%d+),? (%d+),? (%d+),? [%d%.]+%)'
+      return string.format(
+        '#%02x%02x%02x',
+        tonumber(r),
+        tonumber(g),
+        tonumber(b)
+      )
+    end
+
     return {
       highlighters = {
         fixme = {
@@ -23,25 +61,18 @@ return {
           pattern = '%f[%w]()NOTE()%f[%W]',
           group = 'MiniHipatternsNote',
         },
-        hex_color = hipatterns.gen_highlighter.hex_color(),
-        rgb_color = {
-          pattern = 'rgb%((%d+),? (%d+),? (%d+)%)',
-          group = function(_, match)
-            local r, g, b = match:match 'rgb%((%d+),? (%d+),? (%d+)%)'
-            r, g, b = tonumber(r), tonumber(g), tonumber(b)
-            local hex_color = color_utils.rgbToHex(r, g, b)
-            return hipatterns.compute_hex_color_group(hex_color, 'bg')
-          end,
-        },
-        hsl_color = {
-          pattern = 'hsl%(%d+,? %d+,? %d+%)',
-          group = function(_, match)
-            local h, s, l = match:match 'hsl%((%d+),? (%d+),? (%d+)%)'
-            h, s, l = tonumber(h), tonumber(s), tonumber(l)
-            local hex_color = color_utils.hslToHex(h, s, l)
-            return hipatterns.compute_hex_color_group(hex_color, 'bg')
-          end,
-        },
+        hsl_color = create_color_highlighter(
+          'hsl%(%d+,? %d+,? %d+%)',
+          extract_hsl
+        ),
+        rgb_color = create_color_highlighter(
+          'rgb%(%d+,? %d+,? %d+%)',
+          extract_rgb
+        ),
+        rgba_color = create_color_highlighter(
+          'rgba%(%d+,? %d+,? %d+,? [%d%.]+%)',
+          extract_rgba
+        ),
       },
     }
   end,
