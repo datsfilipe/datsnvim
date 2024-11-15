cfbaca7ac7fa4caeaa146a399f4d2a4096aea780
datsfilipe <datsfilipe.foss@proton.me>
2024-11-15 12:50:35 -0300

    fix: lazy lock file not properly set and remove lazy icons

--- lua/external/nix.lua
+++ lua/external/nix.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.colorscheme = nil
+M.lockfile = nil
 
 local is_nix_colorscheme_set, nix_colorscheme =
   pcall(require, 'utils/nix_colorscheme')
@@ -11,8 +12,6 @@ end
 local is_nix_lazylock_set, nixos_path = pcall(require, 'utils/nix_lazylock')
 if is_nix_lazylock_set then
   M.lockfile = nixos_path
-else
-  M.lockfile = vim.fn.stdpath 'config' .. '/lazy-lock.json'
 end
 
 return M
