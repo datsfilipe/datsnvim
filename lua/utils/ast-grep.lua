8392276e3a7a0d3c5c1a936ff5c31c31edc41cf6
datsfilipe <datsfilipe.foss@proton.me>
2024-11-15 11:43:47 -0300

    refactor: use inner term for ast-grep results fix issue with fish

--- lua/utils/ast-grep.lua
+++ lua/utils/ast-grep.lua
@@ -1,14 +1,26 @@
 local M = {}
 
-local function escape_for_tmux(command)
-  return string.gsub(command, '\'', "'\\''")
+local function escape_for_shell(command)
+  command = string.gsub(command, "'", "'\\''")
+  command = string.gsub(command, '"', '\\"')
+  command = string.gsub(command, '%$', '\\$')
+  command = string.gsub(command, '%(', '\\(')
+  command = string.gsub(command, '%)', '\\)')
+  return command
 end
 
 M.execute = function()
-  local cmd = 'ast-grep ' .. vim.fn.input 'cmd: '
-  local escaped_cmd = escape_for_tmux(cmd)
-  local tmux_cmd = string.format('tmux split-window -h bash -c \'%s; cat\'', escaped_cmd)
-  vim.fn.system(tmux_cmd)
+  local input = vim.fn.input 'ast-grep: '
+  if input == nil or input == '' then
+    print 'No input provided.'
+    return
+  end
+
+  local cmd = 'ast-grep ' .. input
+  local escaped_cmd = escape_for_shell(cmd)
+  local split_cmd =
+    string.format("vsplit | terminal fish -c '%s; cat'", escaped_cmd)
+  vim.cmd(split_cmd)
 end
 
 return M
