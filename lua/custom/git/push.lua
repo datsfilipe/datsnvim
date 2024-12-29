851fec78636eee14b03dbd5fdda5795b206f88ac
datsfilipe <datsfilipe.foss@proton.me>
2024-12-29 07:19:50 -0300

    refactor: use silent for push command

--- lua/custom/git/push.lua
+++ lua/custom/git/push.lua
@@ -2,17 +2,17 @@ local M = {}
 
 function M.git_push_with_prompt()
   vim.ui.input(
-    { prompt = 'Remote (default: default branch remote): ' },
+    { prompt = 'remote (default: default branch remote): ' },
     function(remote)
       local default_remote = vim.fn
         .system('git symbolic-ref refs/remotes/origin/HEAD --short')
         :match '^%a+/(.+)'
       remote = remote or default_remote
-      vim.ui.input({ prompt = 'Branch (default: current): ' }, function(branch)
+      vim.ui.input({ prompt = 'branch (default: current): ' }, function(branch)
         if not branch then
           branch = vim.fn.system('git branch --show-current'):gsub('\n', '')
         end
-        vim.cmd('!git push ' .. remote .. ' ' .. branch)
+        vim.cmd('silent !git push ' .. remote .. ' ' .. branch)
       end)
     end
   )
