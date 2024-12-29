b0a0852b37516ae1cac446c43a84c88a5e747317
datsfilipe <datsfilipe.foss@proton.me>
2024-12-29 06:38:07 -0300

    chore: remove keybinds from mini diff

--- lua/custom/plugins/ui/diff.lua
+++ lua/custom/plugins/ui/diff.lua
@@ -1,12 +1,5 @@
 return {
   'echasnovski/mini.diff',
   version = false,
-  opts = {
-    mappings = {
-      apply = '<leader>ga',
-      reset = '<leader>gr',
-      goto_next = '<leader>gn',
-      goto_prev = '<leader>gp',
-    },
-  },
+  opts = {},
 }
