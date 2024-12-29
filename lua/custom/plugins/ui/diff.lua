d62f17bd06652dea425a09ff32ba3161ff2ad2e1
datsfilipe <datsfilipe.foss@proton.me>
2024-12-29 20:55:18 -0300

    refactor: move mini diff keymaps to git keymaps

--- lua/custom/plugins/ui/diff.lua
+++ lua/custom/plugins/ui/diff.lua
@@ -1,13 +1,5 @@
 return {
   'echasnovski/mini.diff',
   version = false,
-  enabled = true,
-  opts = {
-    mappings = {
-      apply = '<leader>ga',
-      reset = '<leader>gr',
-      goto_next = '<leader>gn',
-      goto_prev = '<leader>gp',
-    },
-  },
+  opts = {},
 }
