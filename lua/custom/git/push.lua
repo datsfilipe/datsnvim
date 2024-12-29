6ced0ab725be2b5ba47caec60c56e36360e90f44
datsfilipe <datsfilipe.foss@proton.me>
2024-12-29 06:55:56 -0300

    refactor: improve push

--- lua/custom/git/push.lua
+++ lua/custom/git/push.lua
@@ -1,15 +1,21 @@
 local M = {}
 
 function M.git_push_with_prompt()
-  vim.ui.input({ prompt = 'Remote (default: origin): ' }, function(remote)
-    remote = remote or 'origin'
-    vim.ui.input({ prompt = 'Branch (default: current): ' }, function(branch)
-      if not branch then
-        branch = vim.fn.system('git branch --show-current'):gsub('\n', '')
-      end
-      vim.cmd('!git push ' .. remote .. ' ' .. branch)
-    end)
-  end)
+  vim.ui.input(
+    { prompt = 'Remote (default: default branch remote): ' },
+    function(remote)
+      local default_remote = vim.fn
+        .system('git symbolic-ref refs/remotes/origin/HEAD --short')
+        :match '^%a+/(.+)'
+      remote = remote or default_remote
+      vim.ui.input({ prompt = 'Branch (default: current): ' }, function(branch)
+        if not branch then
+          branch = vim.fn.system('git branch --show-current'):gsub('\n', '')
+        end
+        vim.cmd('!git push ' .. remote .. ' ' .. branch)
+      end)
+    end
+  )
 end
 
 return M
