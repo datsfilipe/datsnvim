8c862c00dcf5620cdde82f994ba60c0d2de756c9
datsfilipe <datsfilipe.foss@proton.me>
2024-12-29 06:51:28 -0300

    feat: add back mini diff keybindings

--- lua/custom/plugins/ui/diff.lua
+++ lua/custom/plugins/ui/diff.lua
@@ -1,5 +1,13 @@
 return {
   'echasnovski/mini.diff',
   version = false,
-  opts = {},
+  enabled = true,
+  opts = {
+    mappings = {
+      apply = '<leader>ga',
+      reset = '<leader>gr',
+      goto_next = '<leader>gn',
+      goto_prev = '<leader>gp',
+    },
+  },
 }
