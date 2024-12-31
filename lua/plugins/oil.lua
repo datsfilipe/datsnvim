c81fb317308f02b9bfd7123897086676ab720a9c
datsfilipe <datsfilipe.foss@proton.me>
2024-12-31 19:54:06 -0300

    refactor: update oil keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,12 +3,11 @@ return {
   lazy = false,
   keys = {
     { '<leader>e', '<cmd>Oil<cr>' },
-    { '<leader>r', '<cmd>edit %:p:h<cr>' },
   },
   opts = {
     default_file_explorer = true,
     keymaps = {
-      ['<leader>H'] = 'actions.select_split',
+      ['<leader>v'] = 'actions.select_split',
     },
     view_options = {
       show_hidden = true,
