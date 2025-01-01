0f0bb4a85ce43e1c80712152c5153f8e04ceb9ec
datsfilipe <datsfilipe.foss@proton.me>
2025-01-01 04:49:34 -0300

    feat: add quickfix list config

---
+++ plugin/quickfix_list.lua
@@ -0,0 +1,23 @@
+local function toggle_qf()
+  local win_id = vim.fn.getqflist({ winid = 0 }).winid
+  if win_id ~= nil and win_id ~= 0 then
+    vim.cmd 'ccl'
+  else
+    vim.cmd 'copen'
+    vim.cmd 'wincmd p'
+  end
+end
+
+vim.keymap.set('n', ';e', toggle_qf)
+vim.keymap.set(
+  'n',
+  '<C-p>',
+  '<cmd>cprev<CR>zz<cmd>lua print("qflist: prev")<CR>'
+)
+vim.keymap.set(
+  'n',
+  '<C-n>',
+  '<cmd>cnext<CR>zz<cmd>lua print("qflist: next")<CR>'
+)
+
+vim.keymap.set('n', ';E', '<cmd>call setqflist([], "r")<CR><cmd>ccl<CR>')
