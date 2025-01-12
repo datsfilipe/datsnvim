e4eea27a13c4fa29ff610c6715bb809ca03df4f7
datsfilipe <datsfilipe.foss@proton.me>
2025-01-12 18:12:03 -0300

    feat: add keymap to format a bunch of files on save

--- lua/utils.lua
+++ lua/utils.lua
@@ -13,4 +13,36 @@ M.is_file_available = function(file)
   return path ~= ''
 end
 
+M.format_files_in_dir = function(ext)
+  if not require('utils').is_bin_available 'fd' then
+    print 'fd is not available'
+    return
+  end
+
+  local confirm =
+    vim.fn.input('proceed with formatting ' .. ext .. ' files? [y/n] ')
+  if confirm:lower() ~= 'y' then
+    print 'operation cancelled'
+    return
+  end
+
+  local ignore_dirs = { '.git', 'node_modules', 'target' }
+  local exclude_args = table.concat(
+    vim.tbl_map(function(dir)
+      return '-E ' .. dir
+    end, ignore_dirs),
+    ' '
+  )
+  local cmd = string.format('fd -e %s %s', ext, exclude_args)
+
+  local file_list = vim.fn.systemlist(cmd)
+  if #file_list == 0 then
+    print 'no matching files found'
+    return
+  end
+
+  vim.cmd('args ' .. table.concat(file_list, ' '))
+  vim.cmd 'silent argdo execute "normal gg=G" | update'
+end
+
 return M
