ef3c9efb3c36f7b2febeebe9f857074910f4e3c4
datsfilipe <datsfilipe.foss@proton.me>
2025-01-12 13:40:05 -0300

    feat: add util for finding file

--- lua/utils.lua
+++ lua/utils.lua
@@ -7,6 +7,12 @@ M.is_bin_available = function(bin)
   return path == 1
 end
 
+M.is_file_available = function(file)
+  local root_dir = vim.fn.getcwd()
+  local path = vim.fn.findfile(file, root_dir .. ';')
+  return path ~= ''
+end
+
 function M.hsl2rgb(h, s, l)
   if s == 0 then
     return l, l, l
