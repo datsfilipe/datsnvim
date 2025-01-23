3d5cdf276befd0ffeeb83eb8e675218129d9ab9a
Filipe Lima <datsfilipe.foss@proton.me>
2025-01-22 21:17:06 -0300

    feat: add folding config properly

---
+++ plugin/folding.lua
@@ -0,0 +1,28 @@
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'indent'
+vim.wo.foldtext = ''
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
+
+    if
+      client:supports_method(vim.lsp.protocol.Methods.textDocument_foldingRange)
+    then
+      vim.o.foldmethod = 'expr'
+      vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter' }, {
+  callback = function()
+    if vim.wo.diff then
+      vim.o.foldmethod = 'diff'
+      vim.o.foldlevelstart = 0
+    end
+  end,
+})
