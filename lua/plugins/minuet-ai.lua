01f091f2be21324480c175643948fc540b749a8a
Filipe Lima <datsfilipe.foss@proton.me>
2025-03-30 07:30:53 -0300

    chore: replace supermaven with local llm too

---
+++ lua/plugins/minuet-ai.lua
@@ -0,0 +1,42 @@
+vim.g.minuet_virtualtext_enabled = false
+
+return {
+  'milanglacier/minuet-ai.nvim',
+  event = 'InsertEnter',
+  version = '*',
+  config = function()
+    if not vim.g.minuet_virtualtext_enabled then
+      vim.api.nvim_create_autocmd('InsertEnter', {
+        pattern = '*',
+        command = 'Minuet virtualtext enable',
+        once = true,
+      })
+      vim.g.minuet_virtualtext_enabled = true
+    end
+
+    require('minuet').setup {
+      provider = 'openai_fim_compatible',
+      n_completions = 1,
+      context_window = 16000,
+      provider_options = {
+        openai_fim_compatible = {
+          api_key = 'TERM',
+          name = 'ollama',
+          end_point = 'http://localhost:11434/v1/completions',
+          model = 'qwen2.5-coder:7b',
+          optional = {
+            max_tokens = 56,
+            top_p = 0.9,
+          },
+        },
+      },
+      virtualtext = {
+        show_on_completion_menu = false,
+        keymap = {
+          accept = '<C-g>',
+          dismiss = '<C-e>',
+        },
+      },
+    }
+  end,
+}
