28e95c468df7429532b6a21618862c398281b10e
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-12 00:53:01 -0300

    feat: few tweaks on minigit

---
+++ lua/extensions/minigit/init.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.align_blame = function(au_data)
+  if au_data.data.git_subcommand ~= 'blame' then
+    return
+  end
+
+  local win_src = au_data.data.win_source
+  vim.wo.wrap = false
+  vim.fn.winrestview { topline = vim.fn.line('w0', win_src) }
+  vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+
+  vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+end
+
+return M
