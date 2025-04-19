a89a58b38bcb1721a096845b5dde4fb200a05f57
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-19 02:11:39 -0300

    refactor: update autocmds

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -1,9 +1,4 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    if vim.v.event.operator == 'y' then
-      for i = 9, 1, -1 do
-        vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
-      end
-    end
-  end,
+vim.api.nvim_create_autocmd('InsertLeave', {
+  pattern = '*',
+  command = 'set nopaste',
 })
