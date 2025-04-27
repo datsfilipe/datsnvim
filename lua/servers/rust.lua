2de3c0a201819234f74ca3fcc80290c883760a00
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-27 02:14:58 -0300

    fix: correct rust server and bit refactor

--- lua/servers/rust.lua
+++ lua/servers/rust.lua
@@ -1,10 +1,10 @@
 local utils = require 'utils'
-if not utils.is_bin_available 'rust_analyzer' then
+if not utils.is_bin_available 'rust-analyzer' then
   return
 end
 
 vim.lsp.config.rust_analyzer = {
-  cmd = { 'rust_analyzer' },
+  cmd = { 'rust-analyzer' },
   filetypes = { 'rust' },
   settings = {
     ['rust-analyzer'] = {
