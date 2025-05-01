3f04e24fe12cc5694c1da19642000b59f5142fb3
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-30 23:36:30 -0300

    feat: add more stuff from defaults to ts config

--- lsp/typescript.lua
+++ lsp/typescript.lua
@@ -12,6 +12,7 @@ return {
       },
     },
   },
+  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
   filetypes = {
     'javascript',
     'javascriptreact',
@@ -22,6 +23,20 @@ return {
   },
   init_options = {
     hostInfo = 'neovim',
+  },
+  handlers = {
+    ['_typescript.rename'] = function(_, result, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      vim.lsp.util.show_document({
+        uri = result.textDocument.uri,
+        range = {
+          start = result.position,
+          ['end'] = result.position,
+        },
+      }, client.offset_encoding)
+      vim.lsp.buf.rename()
+      return vim.NIL
+    end,
   },
   on_attach = function(client)
     local source_actions = vim.tbl_filter(function(action)
