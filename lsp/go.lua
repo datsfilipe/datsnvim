7be6b9fd07263f9e1566f34621fca8f63cb32b2a
Filipe Lima <datsfilipe.foss@proton.me>
2025-08-23 18:21:06 -0300

    refactor: use nix to bundle nvim config and install plugins

--- lsp/go.lua
+++
@@ -1,9 +0,0 @@
-local utils = require 'utils'
-if not utils.is_bin_available 'gopls' then
-  return
-end
-
-return {
-  cmd = { 'gopls' },
-  filetypes = { 'go', 'gomod', 'gotmpl', 'gowork' },
-}
---
+++ nvim/lua/plugins/colorschemes/gruvbox.lua
@@ -0,0 +1,18 @@
+local static_color = require('utils').static_color
+
+require('gruvbox.nvim').setup {
+  theme = 'dark',
+  transparent = true,
+  italics = {
+    comments = false,
+    keywords = false,
+    functions = false,
+    strings = false,
+    variables = false,
+  },
+  overrides = vim.tbl_extend('force', {
+    IndentLine = { fg = static_color },
+    IndentLineCurrent = { fg = static_color },
+    NotifyINFOBorder = { fg = static_color },
+  }, {}),
+}
